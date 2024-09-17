0751243993429b565276fc7d2353875c54f6d1fa
Adi <adityasagarpython@gmail.com>
2024-09-17 14:41:34 -0300

    feat: Added navigation to logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,20 +1,22 @@
 import { Navbar, NavbarBrand } from '@nextui-org/navbar'
 import NextLogo from '@/assets/logo-full.png'
 import Image from 'next/image'
-
+import Link from 'next/link'
 export default function TopBar() {
   const menuItemsLoggedInUser = ['Dashboard', 'All Shifts', 'Log Out']
   const menuItems = ['SignIn', 'SignUp']
 
   return (
     <Navbar shouldHideOnScroll className="max-w-[750px] mx-auto">
-      <NavbarBrand>
-        <Image
-          src={NextLogo}
-          alt="placeholder logo"
-          className="max-h-10 w-auto"
-        />
-      </NavbarBrand>
+        <NavbarBrand>
+          <Link href={"/"}>
+            <Image
+              src={NextLogo}
+              alt="placeholder logo"
+              className="max-h-10 w-auto"
+            />
+          </Link>
+        </NavbarBrand>
     </Navbar>
   )
 }
